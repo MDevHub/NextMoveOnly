3f504aa862be0bb999501a8748697d285c61f15f
MDevHub <musbaudeenabdulrahman040@gmail.com>
2025-05-24 09:16:51 +0100

    Fixed the body

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,11 +3,8 @@ import  './Navbar.css'
 const Navbar = () => {
   return (
     <div className='nav'>
-      <div className="nav-logo">EV-olution</div>
+      <div className="nav-logo">Next Move Only</div>
       <ul className="nav-menu">
-         <li>Home</li>
-         <li>Explore</li>
-         <li>About</li>
          <li className='nav-contact' id='nav-c'><a href='mailto:musbaudeenabdulrahman040@gmail.com'>Contact-Dev</a></li>
       </ul>
     </div>
